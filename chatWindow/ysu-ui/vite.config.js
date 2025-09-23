1e707cd8500f21c4ad7710f17277b6723ffe6f74
SZH <3401099378@qq.com>
2025-09-23 19:55:13 +0800

    前端第一次提交：基本界面

---
+++ chatWindow/ysu-ui/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue()
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: ' http://47.109.39.124:8090',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // 去掉 /api 前缀
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
